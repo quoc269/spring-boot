bfec281408847e39320fbd1702b5294f60e4849a
Quoc <quoc2@DESKTOP-AA716SV>
2020-03-31 21:43:33 +0700

    met ghe

--- quoc-springboot/src/main/java/quoc/QuocSpringbootApplication.java
+++ quoc-springboot/src/main/java/quoc/QuocSpringbootApplication.java
@@ -3,7 +3,7 @@ package quoc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//i love you, ms N
+//i love you, ms N tui da thaay doi ne
 @SpringBootApplication
 public class QuocSpringbootApplication {
 
